b064694fb13e69a26f4e607afb779b4e2ff9e648
Subash B <subash59245@gmail.com>
2025-04-18 20:19:01 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -531,5 +531,4 @@ def create_dummy_admin():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
